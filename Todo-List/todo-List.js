ac504592fd1a1a73f8df0096bd71f3a8944b8a62
Ajith Kumar S <ajithkumars1609@gmail.com>
2023-10-11 16:19:39 +0530

    todoList delete button and dude date is created and tested successfully

--- Todo-List/todo-List.js
+++ Todo-List/todo-List.js
@@ -1,18 +1,44 @@
-const todoList = [];
+const todoList = [
+  {
+    name: "make food",
+    dueDate: "2022-09-21",
+  },
+  {
+    name: "watch youtube",
+    dueDate: "2022-09-21",
+  },
+];
 function renderTodo() {
   let todoHtml = "";
   for (let i = 0; i <= todoList.length - 1; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = `<p>
+      ${name}
+      ${dueDate}
+      <button onclick="
+        todoList.splice(${i},1);
+        renderTodo();
+      ">Delete</button>
+    </p>`;
     todoHtml += html;
-    document.querySelector(".js-display-todo-list").innerHTML = todoHtml;
   }
+  document.querySelector(".js-display-todo-list").innerHTML = todoHtml;
 }
 function todoAdd() {
   const inputElement = document.querySelector(".js-input-button");
-  const inputValue = inputElement.value;
-  todoList.push(inputValue);
+  const name = inputElement.value;
+  const dueDateElment = document.querySelector(".js-due-date-input");
+  const dueDate = dueDateElment.value;
   inputElement.value = "";
+  dueDateElment.value = "";
+  todoList.push({
+    name: name,
+    dueDate: dueDate,
+    // name
+    // dueDate // simple variable and same //
+  });
   renderTodo();
 }
 function enterButton(event) {
